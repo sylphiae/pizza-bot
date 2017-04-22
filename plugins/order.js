9fe712136e46d356bff590a473a9b8de42a32b3a
Jenny Tong <j.tong89@gmail.com>
2017-04-22 11:14:34 -0500

    Allow user to specify an address

--- plugins/order.js
+++ plugins/order.js
@@ -3,18 +3,30 @@ const pizzapi = require('dominos')
 module.exports = {
   init: (controller) => {
     controller.hears([/I want a pizza/], ['direct_message', 'direct_mention'], (bot, message) => {
-      bot.reply(message, `I am looking for nearby stores!`)
-      pizzapi.Util.findNearbyStores('11 Times Square, New York, NY 10036', 'Delivery', (storeData) => {
-      	var storesInfo = storeData.result.Stores.filter(store => {
-      		return store.IsOnlineNow && store.IsOnlineCapable && store.IsOpen
-      	}).map(store => {
-      		var address = store.AddressDescription.split('\n')
-      		address = address[0] + ' ' + address[1] + '\n'
-      		return store.StoreID + address
-      	})
-      	.join(' ')
-      	bot.reply(message, 'I have found these stores: ' + '\r\n' + storesInfo)
-      })
+      bot.createConversation(message, setUpConvo)
+
+      function setUpConvo(err, convo) {
+      	convo.addMessage({text: `I will find you a pizza!`, action: 'thread_2'}, 'thread_1')
+      	convo.addQuestion('Where do you want the pizza to be delivered?', (responseObj) => {
+      		convo.setVar('address', responseObj.text)
+      		console.log(responseObj)
+      		pizzapi.Util.findNearbyStores(responseObj.text, 'Delivery', (storeData) => {
+		      	var storesInfo = storeData.result.Stores.filter(store => {
+		      		return store.IsOnlineNow && store.IsOnlineCapable && store.IsOpen
+		      	}).map(store => {
+		      		var address = store.AddressDescription.split('\n')
+		      		address = address[0] + ' ' + address[1] + '\n'
+		      		return store.StoreID + address
+		      	}).join(' ')
+		      	bot.reply(message, 'I have found these stores: ' + '\r\n' + storesInfo)
+      		});
+      		convo.gotoThread('thread_3')
+      	}, {}, 'thread_2')
+      	convo.addMessage('Here is your address: {{ vars.address }}', 'thread_3')
+
+      	convo.activate()
+      	convo.gotoThread('thread_1')
+      };
     })
   },
   help: {
